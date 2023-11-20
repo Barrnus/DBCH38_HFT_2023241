95038da742710d9ca5e3df48a09c6ed64ee51e4d
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-21 00:55:09 +0100

    fixed dbcontext and nugets

---
+++ DBCH38_HFT_2023241.Repository/TaskDbContext.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.EntityFrameworkCore;
+using DBCH38_HFT_2023241.Models;
+using System;
+
+namespace DBCH38_HFT_2023241.Repository
+{
+    public class TaskDbContext : DbContext
+    {
+    }
+}
