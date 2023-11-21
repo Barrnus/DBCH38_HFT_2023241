387bb0eba28d99df6647be492b631cd205128dba
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-21 02:45:31 +0100

    implemented task logic

--- DBCH38_HFT_2023241.Logic/TaskLogic.cs
+++ DBCH38_HFT_2023241.Logic/TaskLogic.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DBCH38_HFT_2023241.Models;
+using DBCH38_HFT_2023241.Repository;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,42 @@ using System.Threading.Tasks;
 
 namespace DBCH38_HFT_2023241.Logic
 {
-    public class TaskLogic
+    public class TaskLogic : ITaskLogic
     {
+        ITaskRepository<Models.Task> repo;
+
+        public TaskLogic(ITaskRepository<Models.Task> repo)
+        {
+            this.repo = repo;
+        }
+        public void Create(Models.Task task)
+        {
+            repo.Create(task);
+        }
+
+        public void Delete(int id)
+        {
+            repo.Delete(id);
+        }
+
+        public Models.Task Read(int id)
+        {
+            Models.Task task = repo.Read(id);
+            if (task == null)
+            {
+                throw new ArgumentException("task with given id does not exist");
+            }
+            return task;
+        }
+
+        public IEnumerable<Models.Task> ReadAll()
+        {
+            return repo.ReadAll();
+        }
+
+        public void Update(Models.Task task)
+        {
+            repo.Update(task);
+        }
     }
 }
