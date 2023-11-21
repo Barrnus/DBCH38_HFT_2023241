db7df8efd502d406f4847e43e15a344825d71dc1
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-21 02:16:00 +0100

    implemented prio logic interface

--- DBCH38_HFT_2023241.Logic/IPriorityLogic.cs
+++ DBCH38_HFT_2023241.Logic/IPriorityLogic.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using DBCH38_HFT_2023241.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,10 @@ namespace DBCH38_HFT_2023241.Logic
 {
     public interface IPriorityLogic
     {
+        void Create(Priority priority);
+        void Delete(int id);
+        IEnumerable<Priority> ReadAll();
+        Models.Task Read(int id);
+        void Update(Priority priority);
     }
 }
