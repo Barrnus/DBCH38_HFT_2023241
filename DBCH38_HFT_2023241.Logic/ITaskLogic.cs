3d20b5ab21e47df37714617b3fff0c7620b37e0b
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-21 02:15:05 +0100

    implemented task logic

--- DBCH38_HFT_2023241.Logic/ITaskLogic.cs
+++ DBCH38_HFT_2023241.Logic/ITaskLogic.cs
@@ -8,5 +8,10 @@ namespace DBCH38_HFT_2023241.Logic
 {
     public interface ITaskLogic
     {
+        void Create(Models.Task task);
+        void Delete(int id);
+        IEnumerable<Models.Task> ReadAll();
+        Models.Task Read(int id);
+        void Update(Models.Task task);
     }
 }
