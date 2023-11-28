4de288f09c2420587c61322c6e1928d06c472dba
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-28 13:14:04 +0100

    added required webapi nuget package

--- DBCH38_HFT_2023241.Endpoint/RestService.cs
+++ DBCH38_HFT_2023241.Endpoint/RestService.cs
@@ -1,7 +1,10 @@
-﻿using System.Collections.Generic;
-using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Net;
-using System;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
 
 namespace DBCH38_HFT_2023241.Endpoint
 {
