551a8e11a278f0bf512f03f5882622c1ff5ce06c
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-28 13:25:11 +0100

    reinit of controllers

--- DBCH38_HFT_2023241.Endpoint/Controllers/PriorityController.cs
+++ DBCH38_HFT_2023241.Endpoint/Controllers/PriorityController.cs
@@ -1,10 +1,83 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 
 namespace DBCH38_HFT_2023241.Endpoint.Controllers
 {
-    [Route("api/[controller]")]
-    [ApiController]
-    public class PriorityController :ControllerBase
+    public class PriorityController : Controller
     {
+        // GET: PriorityController
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: PriorityController/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: PriorityController/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: PriorityController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PriorityController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: PriorityController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: PriorityController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: PriorityController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
