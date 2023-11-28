b092364b7fe51c9895f201d84a54a1e6355746bf
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-28 13:31:45 +0100

    implemented worker controller

--- DBCH38_HFT_2023241.Endpoint/Controllers/WorkerController.cs
+++ DBCH38_HFT_2023241.Endpoint/Controllers/WorkerController.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using DBCH38_HFT_2023241.Logic;
+using DBCH38_HFT_2023241.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
 
 namespace DBCH38_HFT_2023241.Endpoint.Controllers
 {
@@ -7,6 +10,40 @@ namespace DBCH38_HFT_2023241.Endpoint.Controllers
     [ApiController]
     public class WorkerController : ControllerBase
     {
-        
+        IWorkerLogic logic;
+        public WorkerController(IWorkerLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Worker> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Worker Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Worker worker)
+        {
+            this.logic.Create(worker);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Worker worker)
+        {
+            this.logic.Update(worker);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
