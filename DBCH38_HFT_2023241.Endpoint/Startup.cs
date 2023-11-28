22e85e6e3c38e0a9c45b894787cdac3d07c0f2bc
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-28 13:41:39 +0100

    set ConfigureServices

--- DBCH38_HFT_2023241.Endpoint/Startup.cs
+++ DBCH38_HFT_2023241.Endpoint/Startup.cs
@@ -1,3 +1,6 @@
+using DBCH38_HFT_2023241.Logic;
+using DBCH38_HFT_2023241.Models;
+using DBCH38_HFT_2023241.Repository;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
@@ -16,6 +19,17 @@ namespace DBCH38_HFT_2023241.Endpoint
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton<TaskDbContext>();
+
+            services.AddTransient<IPriorityRepository<Priority>, PriorityRepository>();
+            services.AddTransient<ITaskRepository<Models.Task>, TaskRepository>();
+            services.AddTransient<IWorkerRepository<Worker>, WorkerRepository>();
+
+            services.AddTransient<IPriorityLogic, PriorityLogic>();
+            services.AddTransient<ITaskLogic, TaskLogic>();
+            services.AddTransient<IWorkerLogic, WorkerLogic>();
+
+            services.AddControllers();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
