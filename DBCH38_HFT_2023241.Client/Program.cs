f566174a3111b416a43b3b49c9d7f24af7959a72
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-28 14:10:24 +0100

    fixed webapi nuget package

--- DBCH38_HFT_2023241.Client/Program.cs
+++ DBCH38_HFT_2023241.Client/Program.cs
@@ -5,6 +5,8 @@ namespace DBCH38_HFT_2023241.Client
     {
         static void Main(string[] args)
         {
+            RestService rest = new RestService("http://localhost:5000");
+
 
             //TODO: ENDPOINT
             //TODO: 5 noncrud
