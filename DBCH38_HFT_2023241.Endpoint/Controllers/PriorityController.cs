eb91d865da54975b7e74e14cb5e3900e5d186abe
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-28 13:30:54 +0100

    implemented priority controller

--- DBCH38_HFT_2023241.Endpoint/Controllers/PriorityController.cs
+++ DBCH38_HFT_2023241.Endpoint/Controllers/PriorityController.cs
@@ -10,6 +10,40 @@ namespace DBCH38_HFT_2023241.Endpoint.Controllers
     [ApiController]
     public class PriorityController : ControllerBase
     {
-        
+        IPriorityLogic logic;
+        public PriorityController(IPriorityLogic logic)
+        {
+            this.logic = logic;
+        }
+
+        [HttpGet]
+        public IEnumerable<Priority> ReadAll()
+        {
+            return this.logic.ReadAll();
+        }
+
+        [HttpGet("{id}")]
+        public Priority Read(int id)
+        {
+            return this.logic.Read(id);
+        }
+
+        [HttpPost]
+        public void Create([FromBody] Priority priority)
+        {
+            this.logic.Create(priority);
+        }
+
+        [HttpPut]
+        public void Put([FromBody] Priority priority)
+        {
+            this.logic.Update(priority);
+        }
+
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            this.logic.Delete(id);
+        }
     }
 }
