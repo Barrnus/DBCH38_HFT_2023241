d2242a68fea012ada6f4facad54f674d8ef81358
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-11-29 05:15:08 +0100

    init of consolemenu crud methods

--- DBCH38_HFT_2023241.Client/Program.cs
+++ DBCH38_HFT_2023241.Client/Program.cs
@@ -3,6 +3,71 @@ namespace DBCH38_HFT_2023241.Client
 {
     internal class Program
     {
+        static void Create(string entity)
+        {
+            switch (entity)
+            {
+                case "Task":
+                    break;
+                case "Priority":
+                    break;
+                case "Worker":
+                    break;
+            }
+        }
+
+        static void ReadAll(string entity)
+        {
+            switch (entity)
+            {
+                case "Task":
+                    break;
+                case "Priority":
+                    break;
+                case "Worker":
+                    break;
+            }
+        }
+
+        static void Read(string entity)
+        {
+            switch (entity)
+            {
+                case "Task":
+                    break;
+                case "Priority":
+                    break;
+                case "Worker":
+                    break;
+            }
+        }
+
+        static void Update(string entity)
+        {
+            switch (entity)
+            {
+                case "Task":
+                    break;
+                case "Priority":
+                    break;
+                case "Worker":
+                    break;
+            }
+        }
+
+        static void Delete (string entity) 
+        {
+            switch (entity)
+            {
+                case "Task":
+                    break;
+                case "Priority":
+                    break;
+                case "Worker":
+                    break;
+            }
+        }
+
         static void Main(string[] args)
         {
             RestService rest = new RestService("http://localhost:1542/");
