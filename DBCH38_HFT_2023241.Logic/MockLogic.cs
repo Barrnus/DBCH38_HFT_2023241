acfa27cbd6db9467244f5f4f5773901c45c6cacf
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-12-06 18:19:48 +0100

    added mocklogic for unit tests

---
+++ DBCH38_HFT_2023241.Logic/MockLogic.cs
@@ -0,0 +1,27 @@
+﻿using DBCH38_HFT_2023241.Models;
+using DBCH38_HFT_2023241.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DBCH38_HFT_2023241.Logic
+{
+    public class MockLogic
+    {
+        ITaskRepository<Models.Task> taskRepo;
+        IWorkerRepository<Worker> workerRepo;
+        IPriorityRepository<Priority> prioRepo;
+        public MockLogic(ITaskRepository<Models.Task> taskRepo,IWorkerRepository<Worker> workerRepo,IPriorityRepository<Priority> prioRepo)
+        {
+            TaskRepo = taskRepo;
+            WorkerRepo = workerRepo;
+            PrioRepo = prioRepo;
+        }
+
+        public ITaskRepository<Models.Task> TaskRepo { get => taskRepo; private set => taskRepo = value; }
+        public IWorkerRepository<Worker> WorkerRepo { get => workerRepo; private set => workerRepo = value; }
+        public IPriorityRepository<Priority> PrioRepo { get => prioRepo; private set => prioRepo = value; }
+    }
+}
