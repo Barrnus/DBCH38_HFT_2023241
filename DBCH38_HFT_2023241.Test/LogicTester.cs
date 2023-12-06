b9cebace78a161619919f31bd8318e935ebdc9a2
Barrnuss <barcsi.barnabas@stud.uni-obuda.hu>
2023-12-06 18:20:17 +0100

    init test class

---
+++ DBCH38_HFT_2023241.Test/LogicTester.cs
@@ -0,0 +1,19 @@
+﻿using DBCH38_HFT_2023241.Logic;
+using DBCH38_HFT_2023241.Models;
+using DBCH38_HFT_2023241.Repository;
+using Moq;
+using NUnit.Framework;
+using System;
+
+namespace DBCH38_HFT_2023241.Test
+{
+    [TestFixture]
+    public class LogicTester
+    {
+        MockLogic logic;
+        Mock<ITaskRepository<Models.Task>> mockTaskRepo;
+        Mock<IWorkerRepository<Worker>> mockWorkerRepo;
+        Mock<IPriorityRepository<Priority>> mockPrioRepo;
+
+    }
+}
